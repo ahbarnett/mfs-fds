7c8ea1682a06852a0c73f2afebc14f177b0fa860
Yuxiang Liu <larryliu1991@gmail.com>
2021-04-19 22:01:00 -0500

    added LineWidth in the plotting script

--- 2D/PlottingResults.m
+++ 2D/PlottingResults.m
@@ -4,32 +4,32 @@ title('err vs N,d when k = 300');
 
 figure; 
 load('scat_k30.mat'); 
-semilogy(Ns, err); set(gca, 'FontSize', 16); 
+semilogy(Ns, err, 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('err vs N when k = 30'); 
 figure; 
-plot(Ns, ts'); set(gca, 'FontSize', 16); 
+plot(Ns, ts', 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('t(s) vs N when k = 30'); 
 
 figure; 
 load('scat_k300.mat'); 
-semilogy(Ns, err); set(gca, 'FontSize', 16); 
+semilogy(Ns, err, 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('err vs N when k = 300'); 
 figure; 
-plot(Ns, ts'); set(gca, 'FontSize', 16); 
+plot(Ns, ts', 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('t(s) vs N when k = 300'); 
 
 figure; 
 load('scat_k3000.mat');
-semilogy(Ns, err); set(gca, 'FontSize', 16); 
+semilogy(Ns, err, 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('err vs N when k = 3000'); 
 figure; 
-plot(Ns, ts'); set(gca, 'FontSize', 16); 
+plot(Ns, ts', 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('t(s) vs N when k = 3000'); 
 
 figure; 
 load('scat_N1e5_ks.mat'); 
-semilogy(ks, err); set(gca, 'FontSize', 16); 
+semilogy(ks, err, 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('err vs k when N = 1e5'); 
 figure; 
-plot(ks, ts'); set(gca, 'FontSize', 16); 
+plot(ks, ts', 'LineWidth', 1.5); set(gca, 'FontSize', 16); 
 title('t(s) vs N when N = 1e5'); 
